fbd9514eaf4a96d342b78e4fa0535c95ef52b328
Chris <chris@schoolofcode.co.uk>
2020-03-09 10:03:33 +0000

    Initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import css from "./App.module.css";
+
+function App() {
+  return (
+    <div className={css.container}>
+      <h1>Happy Hacking!</h1>
+    </div>
+  );
+}
+
+export default App;
