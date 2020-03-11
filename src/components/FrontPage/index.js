2cb08f0a83434723b9f99e99b39a33a8016d0fb1
Tao Sharma <taosharma@gmail.com>
2020-03-11 10:07:43 +0000

    testingWithBackend

--- src/components/FrontPage/index.js
+++ src/components/FrontPage/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Link } from "react-router-dom";
 
 import Banner from "../Banner";
 import Content from "../Content";
