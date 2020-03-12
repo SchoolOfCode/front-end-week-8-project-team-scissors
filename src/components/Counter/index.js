3d48ce9d9d79b5ac107a6d4aa51c30ee3516f4eb
Grace <gracebarata@gmail.com>
2020-03-12 14:31:46 +0000

    front page new changes

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-function Counter({ grid }) {
+function Counter({ grid, counterText }) {
   const [counter, setCounter] = useState(1045);
   const randomTime = Math.floor(Math.random() * 10000);
   console.log(randomTime);
@@ -14,7 +14,10 @@ function Counter({ grid }) {
   }, [counter]);
   return (
     <section className={grid}>
-      <h1>{counter}</h1>
+      <h1 className={counterText}>
+        Together we have planted{" "}
+        <span style={{ color: "#f58025" }}>{counter}</span> trees!
+      </h1>
     </section>
   );
 }
--- src/components/FrontPage/index.js
+++ src/components/FrontPage/index.js
@@ -11,14 +11,21 @@ import css from "./FrontPage.module.css";
 
 const banner = {
   type: "banner",
-  heading:
-    "Some motivating and inspired call to action. blah blah we need to plant trees to combat climate change and lead the way for a new green revolution"
+  heading: "Plant a Tree. Let's lead a new green revolution"
 };
 
-const content = {
+const content1 = {
   type: "content",
-  title: "benefits blah",
-  text: "here are the benefits of planting trees blah blah blah"
+  title: "Benefit 1",
+  text:
+    "One tree can absorb 1 ton of carbon dioxide in 40 years. That's up to 48 pounds a year! This improves the air quality of the surrounding area."
+};
+
+const content2 = {
+  type: "content",
+  title: "Benefit 2",
+  text:
+    "A tree's leaves absorb and block sound. Lining a street with trees has reduced noise pollution by as much as 40% in some areas! Tree canopies (the leafy part at the very top) also cool down streets. The evaporation from one tree has the effect of 10 air-coolers."
 };
 
 const portal = { type: "portal", link: "insert link here" };
@@ -27,10 +34,10 @@ function FrontPage() {
   return (
     <section className={css.frontPage}>
       <Banner {...banner} />
-      <Content {...content} grid={css.contentGrid1} />
-      <Content {...content} grid={css.contentGrid2} />
-      <Content {...content} grid={css.contentGrid3} />
-      <Counter grid={css.counterGrid1} />
+      <Content {...content1} grid={css.contentGrid1} />
+      <Content {...content2} grid={css.contentGrid2} />
+      <Content grid={css.contentGrid3} />
+      <Counter grid={css.counterGrid1} counterText={css.counterText} />
       <Link to="/Maps">
         <Portal grid={css.portalGrid1} {...portal} />
       </Link>
