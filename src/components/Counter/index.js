0f17adda66b0694e2a58fb0181a924c39dad4c05
Tao Sharma <taosharma@gmail.com>
2020-03-12 17:00:09 +0000

    counterWORKING

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,33 +1,9 @@
 import React, { useState, useEffect } from "react";
 
-<<<<<<< HEAD
 function Counter({ grid, counterText }) {
-  const [counter, setCounter] = useState(1045);
+  const [counter, setCounter] = useState(1457);
   const randomTime = Math.floor(Math.random() * 10000);
-  console.log(randomTime);
-=======
-function Counter() {
-  const [counter, setCounter] = useState(0);
-  const randomTime = Math.floor(Math.random() * 10000);
-  console.log(randomTime);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter(counter => counter + 1);
-    }, randomTime);
-    return () => {
-      clearInterval(interval);
-    };
-  }, [counter]);
-  return <h1>{counter}</h1>;
-}
 
-export default Counter;
-
-/* CODE TO BE USED TO FETCH ACTUAL TREE TOTAL FROM THE DATABASE
-function Counter({ grid }) {
-  const [counter, setCounter] = useState(0);
-
->>>>>>> 51b0ea835e0cc948b86da9cd71a59c7657528691
   useEffect(() => {
     const interval = setInterval(() => {
       setCounter(counter => counter + 1);
@@ -39,10 +15,11 @@ function Counter({ grid }) {
   return (
     <section className={grid}>
       <h1 className={counterText}>
-        Together we have planted{" "}
+        Together we have planted:
         <span style={{ color: "#f58025" }}>{counter}</span> trees!
       </h1>
     </section>
   );
 }
-*/
+
+export default Counter;
